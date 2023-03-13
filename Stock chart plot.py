76bf99db69a771943b880e703c9950cd3017ba3e
Ameerraja <52620618+imameer@users.noreply.github.com>
2023-03-13 22:17:16 +0530

    Add files via upload

---
+++ Stock chart plot.py
@@ -0,0 +1,10 @@
+from nsepy import get_history
+from datetime import date
+
+tikcer = input("enter stock name: ")
+data = get_history(symbol=tikcer, start=date(2023,1,1), end=date(2023,3,9))
+data[['Close']].plot()
+
+
+
+
