38b8b20d06c43acc8da68ca7c402a46ce6679a86
imameer <ameerrajaeee@gmail.com>
2023-03-13 22:01:30 +0530

    First commit

---
+++ Stock chart plot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from nsepy import get_history
+from datetime import date
+tikcer = input("enter stock name: ")
+data = get_history(symbol=tikcer, start=date(2023,1,1), end=date(2023,3,9))
+data[['Close']].plot()
+
+
+# In[ ]:
+
+
+
+
